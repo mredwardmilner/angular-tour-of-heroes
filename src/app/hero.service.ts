4a44b9ee8ea50f6a22b5ca7a0af09c8c70182bd4
Edward Milner <edward.milner@techdept.co.uk>
2017-10-19 16:02:20 +0100

    Finish HTTP API setup

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,12 @@
 // The naming convention for service files is the service name in lowercase followed by .service. For a multi-word service name, use lower dash-case.
 
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+// There are many operators like toPromise that extend Observable with useful capabilities. To use those capabilities, you have to add the operators themselves. That's as easy as importing them from the RxJS library like this:
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 
 // The @Injectable() decorator tells TypeScript to emit metadata about the service. The metadata specifies that Angular may need to inject other dependencies into this service.
@@ -10,16 +14,38 @@ import { HEROES } from './mock-heroes';
 
 
 export class HeroService {
+    private heroesUrl = 'api/heroes';  // URL to web api
+
+    constructor(private http: Http) { }
+
     // A Promise essentially promises to call back when the results are ready. You ask an asynchronous service to call the function with the results or an error.
 
     getHero(id: number): Promise<Hero> {
-        return this.getHeroes()
-            .then(heroes => heroes.find(hero => hero.id === id));
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get(url)
+          .toPromise()
+          .then(response => response.json().data as Hero) //The data in the response is a single hero object rather than an array.
+          .catch(this.handleError);
     };
 
+    // The data should successfully load from the mock server.
+
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            // The Angular http.get returns an RxJS Observable. Observables are a powerful way to manage asynchronous data flows.
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            // The Promise's then() callback, you call the json method of the HTTP Response to extract the data within the response.
+            // The response JSON has a single data property, which holds the array of heroes that the caller wants. So you grab that array and return it as the resolved Promise value.
+            .catch(this.handleError);
     }; // This is called a stub
+
+    // You must anticipate HTTP failures, as they happen frequently for reasons beyond your control.
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+        // The code includes an error to the caller in a rejected promise, so that the caller can display a proper error message to the user.
+    }
 
     getHeroesSlowly(): Promise<Hero[]> { // Simulate slow connection
         return new Promise(resolve => {
